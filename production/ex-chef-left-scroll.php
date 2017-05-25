8399d2cc3874f3928752fd7305e613ed766fad5a
yohanranuk <yohanranuk@gmail.com>
2017-05-26 02:02:56 +0530

    Added the top nav and left scroll

--- production/ex-chef-left-scroll.php
+++ production/ex-chef-left-scroll.php
@@ -5,6 +5,7 @@
 
     <div class="clearfix"></div>
 
+
     <!-- menu profile quick info -->
     <div class="profile clearfix">
         <div class="profile_pic">
---
+++ production/ex-chef-top-nav.php
@@ -0,0 +1,94 @@
+<div class="top_nav">
+    <div class="nav_menu">
+        <nav>
+            <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                    <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown"
+                       aria-expanded="false">
+                        <img src="images/img.jpg" alt="">Mr.W.Weerasekara
+                        <span class=" fa fa-angle-down"></span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-usermenu pull-right">
+                        <li>
+                            <a href="javascript:;">
+                                <span class="badge bg-red pull-right"></span>
+                                <span>Settings</span>
+                            </a>
+                        </li>
+                        <li><a href="index.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                    </ul>
+                </li>
+
+                <li role="presentation" class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown"
+                       aria-expanded="false">
+                        <i class="fa fa-envelope-o"></i>
+                        <span class="badge bg-green">4</span>
+                    </a>
+                    <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                        <li>
+                            <a>
+                                <span class="image"><img src="images/img.jpg" alt="Profile Image"/></span>
+                                <span>
+                          <span>System admin</span>
+                          <span class="time"> 1 min ago</span>
+                        </span>
+                                <span class="message">
+                            New menu item have been added
+                        </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <span class="image"><img src="images/img.jpg" alt="Profile Image"/></span>
+                                <span>
+                          <span>System admin </span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                                <span class="message">
+                          New Staff member have been added
+                        </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <span class="image"><img src="images/img.jpg" alt="Profile Image"/></span>
+                                <span>
+                          <span>System admin</span>
+                          <span class="time">5 mins ago</span>
+                        </span>
+                                <span class="message">
+                          New Staff member have been added
+                        </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <span class="image"><img src="images/img.jpg" alt="Profile Image"/></span>
+                                <span>
+                          <span>System admin</span>
+                          <span class="time">2 days ago </span>
+                        </span>
+                                <span class="message">
+                          New menu item have been added
+                        </span>
+                            </a>
+                        </li>
+                        <li>
+                            <div class="text-center">
+                                <a>
+                                    <strong>See All Alerts</strong>
+                                    <i class="fa fa-angle-right"></i>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
